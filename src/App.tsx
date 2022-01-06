daec80a87556d43d4a508bc0d8c1e721f8772197
Wonjun Kang <michael.wonjun.kang@gmail.com>
2022-01-05 19:19:26 -0600

    Add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
 import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
 import GlobalStyle from './styles/globalStyles';
-import MainPage from './pages/MainPage';
 import Layout from './pages/Layout';
-import Navbar from './components/Navbar';
+import LandingPage from './pages/LandingPage';
+import Upload from './features/upload/Upload';
+import Album from './features/album/Album';
+import AlbumList from './features/albumList/AlbumList';
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <Layout navbar={<Navbar />}>
-        <MainPage />
-      </Layout>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="main" element={<Layout />}>
+          <Route path="albums" element={<AlbumList />} />
+          <Route path="albums/:albumTag" element={<Album />} />
+          <Route path="albums/:albumTag/upload" element={<Upload />} />
+        </Route>
+      </Routes>
     </>
   );
 }
