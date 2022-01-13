2d79c045446b83124c5c41af4e034969e8989a64
Wonjun Kang <michael.wonjun.kang@gmail.com>
2022-01-12 22:28:37 -0600

    AlbumList component - handle create new album, edit album name, delete album

--- src/features/albumList/AlbumList.tsx
+++ src/features/albumList/AlbumList.tsx
@@ -1,12 +1,47 @@
 import styled from 'styled-components';
-import { Button } from '@mui/material';
 import AlbumCards from '../../components/mainPage/AlbumCards';
+import albumAPI from '../../apis/albumAPI';
+import FormDialog from '../../components/FormDialog';
+import { useAppDispatch, useAppSelector } from '../../app/hooks';
+import { addNewAlbum, fetchAlbumList, toggleEditMode, setEditModeOff } from './albumListSlice';
+import Button from '@mui/material/Button';
+import { useEffect } from 'react';
 
 export default function AlbumList() {
+  const dispatch = useAppDispatch();
+  const editModeOn = useAppSelector((state) => state.albumList.editModeOn);
+
+  async function handleAlbumCreate(name: string) {
+    const res = await albumAPI.post('/new', { albumName: name });
+    // const {albumId, albumTag, albumName, thumbnailUrl, count} = res.data;
+    dispatch(addNewAlbum(res.data));
+    dispatch(fetchAlbumList());
+  }
+
+  useEffect(() => {
+    dispatch(setEditModeOff());
+  }, []);
+
   return (
     <>
       <ButtonsContainer>
-        <Button variant="contained">New Album</Button>
+        <Button
+          variant={editModeOn ? 'contained' : 'outlined'}
+          color={editModeOn ? 'warning' : 'info'}
+          onClick={() => dispatch(toggleEditMode())}
+        >
+          {editModeOn ? 'Done' : 'Edit'}
+        </Button>
+        {!editModeOn && (
+          <ButtonContainer>
+            <FormDialog
+              buttonText="New Album"
+              dialogTitle="New Album"
+              label="Enter an album name"
+              next={(name: string) => handleAlbumCreate(name)}
+            />
+          </ButtonContainer>
+        )}
       </ButtonsContainer>
       <AlbumsContainer>
         <AlbumCards />
@@ -19,6 +54,10 @@ const ButtonsContainer = styled.div`
   margin-top: 0.5rem;
   display: flex;
   justify-content: end;
+`;
+
+const ButtonContainer = styled.div`
+  margin-left: 0.5rem;
 `;
 
 const AlbumsContainer = styled.div`
