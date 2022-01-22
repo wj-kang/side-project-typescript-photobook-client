85f1dfee7387148da466a2adc1699050f25f506c
Wonjun Kang <michael.wonjun.kang@gmail.com>
2022-01-21 19:37:15 -0600

    Update albumlist slice and component

--- src/features/albumList/AlbumList.tsx
+++ src/features/albumList/AlbumList.tsx
@@ -1,29 +1,28 @@
 import styled from 'styled-components';
 import AlbumCards from '../../components/mainPage/AlbumCards';
-import albumAPI from '../../apis/albumAPI';
 import FormDialog from '../../components/FormDialog';
 import { useAppDispatch, useAppSelector } from '../../app/hooks';
-import { addNewAlbum, fetchAlbumList, toggleEditMode, setEditModeOff } from './albumListSlice';
+import { toggleEditMode, setEditModeOff, createNewAlbum } from './albumListSlice';
 import Button from '@mui/material/Button';
 import { useEffect } from 'react';
+import Spinner from '../../components/Spinner';
 
 export default function AlbumList() {
   const dispatch = useAppDispatch();
-  const editModeOn = useAppSelector((state) => state.albumList.editModeOn);
+  const { editModeOn, status } = useAppSelector((state) => state.albumList);
 
   async function handleAlbumCreate(name: string) {
-    const res = await albumAPI.post('/new', { albumName: name });
-    // const {albumId, albumTag, albumName, thumbnailUrl, count} = res.data;
-    dispatch(addNewAlbum(res.data));
-    dispatch(fetchAlbumList());
+    dispatch(createNewAlbum(name));
   }
 
   useEffect(() => {
-    dispatch(setEditModeOff());
+    if (editModeOn) dispatch(setEditModeOff());
   }, []);
 
   return (
     <>
+      {status === 'loading' ? <Spinner backgroundColor="rgb(0, 0, 0, 0.4)" /> : null}
+      <Title>My Albums</Title>
       <ButtonsContainer>
         <Button
           variant={editModeOn ? 'contained' : 'outlined'}
@@ -49,6 +48,14 @@ export default function AlbumList() {
     </>
   );
 }
+const Title = styled.h1`
+  width: 100%;
+  text-align: center;
+  line-height: 100%;
+
+  font-size: 1.625rem;
+  color: #505050;
+`;
 
 const ButtonsContainer = styled.div`
   margin-top: 0.5rem;
--- src/features/albumList/albumListSlice.ts
+++ src/features/albumList/albumListSlice.ts
@@ -1,12 +1,23 @@
 import { createSlice, createAsyncThunk, PayloadAction } from '@reduxjs/toolkit';
-import axios from 'axios';
-import { AlbumInfos, AlbumInfo } from './albumListAPI';
+import albumAPI from '../../apis/albumAPI';
+
+type AlbumInfo = {
+  albumId: number;
+  albumTag: string;
+  albumName: string | null;
+  thumbnailUrl: string;
+  count: number;
+};
+
+type AlbumInfos = {
+  [albumId: number]: AlbumInfo;
+};
 
 export interface AlbumListState {
   albumIds: number[];
   albumInfoById: AlbumInfos;
   editModeOn: boolean;
-  status: string | undefined;
+  status: 'loading' | 'succeeded' | 'failed' | undefined;
   error: string | undefined;
 }
 
@@ -18,44 +29,84 @@ const initialState: AlbumListState = {
   error: undefined,
 };
 
+export const createNewAlbum = createAsyncThunk('albumList/newAlbum', async (name: string) => {
+  const res = await albumAPI.post('/new', { albumName: name });
+  return res.data;
+});
+
 export const fetchAlbumList = createAsyncThunk('albumList/getData', async () => {
-  const response = await axios.get(`${process.env.REACT_APP_SERVER_BASE_URL}/album/list`);
+  const response = await albumAPI.get(`/list`);
   return response.data;
 });
+
+export const deleteAlbum = createAsyncThunk('albumList/delete', async (albumId: number) => {
+  await albumAPI.delete(`/${albumId}`);
+  return albumId;
+});
+
+export const editAlbumName = createAsyncThunk(
+  'albumList/edit',
+  async (args: { albumId: number; albumName: string }) => {
+    const { albumId, albumName } = args;
+    await albumAPI.patch(`/${albumId}`, { albumName });
+    return { albumId, albumName };
+  }
+);
 
 const AlbumSlice = createSlice({
   name: 'albumList',
   initialState,
   reducers: {
-    addNewAlbum(state, action: PayloadAction<AlbumInfo>) {
-      const { albumId, albumTag, albumName, thumbnailUrl, count } = action.payload;
-      state.albumIds = [albumId, ...state.albumIds];
-      state.albumInfoById[albumId] = {
-        albumId,
-        albumTag,
-        albumName,
-        thumbnailUrl,
-        count,
-      };
-    },
-    deleteAlbum(state, action: PayloadAction<{ albumId: number }>) {
-      state.albumIds = state.albumIds.filter((id) => id !== action.payload.albumId);
-      state.editModeOn = false;
-    },
-    editAlbumName(state, action: PayloadAction<{ albumId: number; albumName: string | null }>) {
-      const { albumId, albumName } = action.payload;
-      state.albumInfoById[albumId].albumName = albumName;
-      state.editModeOn = false;
-    },
     toggleEditMode(state) {
       state.editModeOn = !state.editModeOn;
     },
     setEditModeOff(state) {
       state.editModeOn = false;
     },
+    setSpinner(state, action: PayloadAction<'loading' | undefined>) {
+      state.status = action.payload;
+    },
   },
   extraReducers(builder) {
     builder
+      .addCase(editAlbumName.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(editAlbumName.fulfilled, (state, action: PayloadAction<{ albumId: number; albumName: string }>) => {
+        const { albumId, albumName } = action.payload;
+        state.status = 'succeeded';
+        state.albumInfoById[albumId].albumName = albumName;
+        state.editModeOn = false;
+      })
+      .addCase(editAlbumName.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message;
+      })
+      .addCase(deleteAlbum.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(deleteAlbum.fulfilled, (state, action: PayloadAction<number>) => {
+        const albumId = action.payload;
+        state.status = 'succeeded';
+        state.albumIds = state.albumIds.filter((id) => id !== albumId);
+      })
+      .addCase(deleteAlbum.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message;
+      })
+      .addCase(createNewAlbum.pending, (state, action) => {
+        state.status = 'loading';
+      })
+      .addCase(createNewAlbum.fulfilled, (state, action: PayloadAction<AlbumInfo>) => {
+        const { albumId, albumTag, albumName, thumbnailUrl, count } = action.payload;
+        state.status = 'succeeded';
+        state.albumIds = [albumId, ...state.albumIds];
+        state.albumInfoById[albumId] = { albumId, albumTag, albumName, thumbnailUrl, count };
+      })
+      .addCase(createNewAlbum.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message;
+      })
       .addCase(fetchAlbumList.pending, (state, action) => {
         state.status = 'loading';
       })
@@ -74,5 +125,5 @@ const AlbumSlice = createSlice({
   },
 });
 
-export const { addNewAlbum, deleteAlbum, editAlbumName, toggleEditMode, setEditModeOff } = AlbumSlice.actions;
+export const { setSpinner, toggleEditMode, setEditModeOff } = AlbumSlice.actions;
 export default AlbumSlice.reducer;
