91d82ff6358c1cdd177bb973cdae396df3513d71
Wonjun Kang <michael.wonjun.kang@gmail.com>
2022-01-21 19:43:23 -0600

    Add account page component - with delete account function

---
+++ src/pages/AccountPage.tsx
@@ -0,0 +1,95 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+import { useAppDispatch, useAppSelector } from '../app/hooks';
+import Spinner from '../components/Spinner';
+import { handleDeleteAccount } from '../features/user/userSlice';
+
+export default function AccountPage() {
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+  const { status, error } = useAppSelector((state) => state.user);
+  const [btnOn, setBtnOn] = useState<boolean>(false);
+  const [deleteConfirmText, setDeleteConfirmText] = useState<string>('');
+  function handleDelete(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    if (!btnOn) return;
+    dispatch(handleDeleteAccount()).then(() => navigate('/'));
+  }
+
+  return (
+    <>
+      {status === 'loading' && <Spinner backgroundColor="rgb(0, 0, 0, 0.4)" />}
+      <Container>
+        <Box>
+          <Title>Delete Account</Title>
+          <Desc>
+            To confirm, type <strong>permanently delete</strong> in the field.
+          </Desc>
+          <Form onSubmit={handleDelete}>
+            <TextInput
+              value={deleteConfirmText}
+              onChange={(e) => {
+                setDeleteConfirmText(e.target.value);
+                e.target.value === 'permanently delete' ? setBtnOn(true) : setBtnOn(false);
+              }}
+              type="text"
+              placeholder="permanently delete"
+            />
+            <Btn type="submit" btnOn={btnOn}>
+              Confirm
+            </Btn>
+          </Form>
+        </Box>
+      </Container>
+    </>
+  );
+}
+
+const Container = styled.div`
+  margin-top: 1rem;
+  width: 100%;
+`;
+
+const Box = styled.div`
+  border: 1px solid #a0a0a0;
+  border-radius: 0.5rem;
+  padding: 2rem 1rem;
+  color: #303030;
+`;
+
+const Title = styled.h1`
+  font-size: 2rem;
+`;
+const Desc = styled.p`
+  margin-top: 0.5rem;
+`;
+
+const Form = styled.form`
+  margin-top: 0.5rem;
+  width: 100%;
+  display: flex;
+`;
+
+const TextInput = styled.input`
+  width: 100%;
+  padding: 0.5rem;
+  border: 1px solid #c0c0c0;
+  border-radius: 0.5rem;
+`;
+
+const Btn = styled.button<{ btnOn: boolean }>`
+  width: 6rem;
+  margin-left: 0.25rem;
+  font-size: 1rem;
+  outline: none;
+  border: 1px solid #c0c0c0;
+  border-radius: 0.5rem;
+  transition: all 300ms ease-in;
+  color: white;
+  cursor: ${(props) => (props.btnOn ? 'pointer' : 'auto')};
+  background: ${(props) => (props.btnOn ? '#850000' : '#c0c0c0')};
+  :hover {
+    ${(props) => (props.btnOn ? 'opacity: 0.8; transform: scale(1.01);' : null)}
+  }
+`;
